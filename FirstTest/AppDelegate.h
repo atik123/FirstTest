c84eadb967b9e7299202f53710855110bb1ef96b
Atik <rahman_uap06@yahoo.com>
2013-12-04 22:47:46 +0600

    Initial Commit

---
+++ FirstTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FirstTest
+//
+//  Created by Atikur Rahman on 12/4/13.
+//  Copyright (c) 2013 Atikur Rahman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
