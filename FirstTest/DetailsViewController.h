a4bdbc18c56311a0bc3266859d3bcc1242aebd6d
Atik <rahman_uap06@yahoo.com>
2013-12-05 01:07:17 +0600

    added details page
    added details page for showing item details information

---
+++ FirstTest/DetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailsViewController.h
+//  FirstTest
+//
+//  Created by Atikur Rahman on 12/5/13.
+//  Copyright (c) 2013 Atikur Rahman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailsViewController : UIViewController
+
+@end
---
+++ FirstTest/DetailsViewController.m
@@ -0,0 +1,38 @@
+//
+//  DetailsViewController.m
+//  FirstTest
+//
+//  Created by Atikur Rahman on 12/5/13.
+//  Copyright (c) 2013 Atikur Rahman. All rights reserved.
+//
+
+#import "DetailsViewController.h"
+
+@interface DetailsViewController ()
+
+@end
+
+@implementation DetailsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
